af3ad021cb2187bf393cc043bd2eb3d97fb6bb04
Michael Martin <michael@rocketpartners.io>
2021-02-10 15:23:34 -0500

    don't collect debug info when not necessary

--- src/main/java/io/rcktapp/api/service/Servlet.java
+++ src/main/java/io/rcktapp/api/service/Servlet.java
@@ -163,6 +163,10 @@ public class Servlet extends HttpServlet
             });
 
          res = new Response();
+         if (!req.isDebug() && !req.isExplain())
+         {
+            res.disableDebugging();
+         }
 
          service.service(req, res);
          writeResponse(req, res, httpResp);
