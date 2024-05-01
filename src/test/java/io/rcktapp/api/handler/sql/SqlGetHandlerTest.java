897c14f82765f3e32b75f1c572de62e89c37fc52
masonchester <masonch32@gmail.com>
2024-05-01 10:18:23 -0400

    Update statics to use try-with-resources and add new test.

--- src/test/java/io/rcktapp/api/handler/sql/SqlGetHandlerTest.java
+++ src/test/java/io/rcktapp/api/handler/sql/SqlGetHandlerTest.java
@@ -65,6 +65,8 @@ public class SqlGetHandlerTest {
 
     @Captor
     ArgumentCaptor<String> sqlCaptor;
+    @Captor
+    ArgumentCaptor<Map<String, String>> rqlParamsCaptor;
 
     @Test
     public void testSqlGetHandler_queryParametersAreProperlyPassedToQueryObjectsMethod() throws Exception {
@@ -74,7 +76,7 @@ public class SqlGetHandlerTest {
         String passedQuery = "updatexml(rand(),concat(CHAR(126),user(),CHAR(126)),null)";
         String collectionName = "plugins";
         String expectedQuery = " SELECT * FROM " + collectionName + " WHERE plugins IN (?,?,?,?,?) ";
-        String [] inClause = {"updatexml(rand()", "concat(CHAR(126)", "user()", "CHAR(126))", "null)"};
+        String[] inClause = {"updatexml(rand()", "concat(CHAR(126)", "user()", "CHAR(126))", "null)"};
 
         // Setup request object and sql table objects to properly reflect
         Request request = new Request(url, "GET", new HashMap<>(), new HashMap<>(), "");
@@ -88,36 +90,102 @@ public class SqlGetHandlerTest {
         collection.setEntity(entity);
         request.setApiMatch(apiMatch);
 
-        mockStatic(Rql.class);
-        mockStatic(Sql.class);
+        // Mocked methods that return local objects used in the future
+        when(chain.getService()).thenReturn(service);
+        when(service.getDb(request.getApi(), request.getCollectionKey(), SqlDb.class)).thenReturn(sqlDb);
+        when(sqlDb.getConnection()).thenReturn(connection);
+
+        try (MockedStatic<Rql> rqlMockedStatic = mockStatic(Rql.class)) {
+            try (MockedStatic<Sql> sqlMockedStatic = mockStatic(Sql.class)) {
+
+                rqlMockedStatic.when(() -> Rql.getRql(sqlDb.getType())).thenReturn(sqlRql);
+
+                when(api.getCollection(request.getCollectionKey(), SqlDb.class)).thenReturn(collection);
+                sqlMockedStatic.when(() -> Sql.getInClauseStr(J.explode(",", Sql.check(request.getEntityKey())))).thenReturn(passedQuery);
+
+                // When we call the service method on the mocked handler actually call the real method
+                doCallRealMethod().when(handler).service(service, api, endpoint, action, chain, request, response);
+
+                // More mocked methods that return objects reused locally
+                when(sqlRql.asCol("plugins")).thenReturn(collectionName);
+                sqlMockedStatic.when(() -> Sql.check(collectionName)).thenReturn(collectionName);
+                sqlMockedStatic.when(() -> Sql.getQuestionMarkStr(inClause)).thenReturn("?,?,?,?,?");
+                when(sqlRql.createStmt(any(), any(), any(), any(Replacer.class))).thenReturn(stmt);
+                when(chain.getConfig("maxRows", 0)).thenReturn(0);
+
+                // Call the actual method on the mocked handler
+                handler.service(service, api, endpoint, action, chain, request, response);
+
+                // Verify that the passed in query contains no SQL functions
+                verify(sqlRql).createStmt(sqlCaptor.capture(), any(), any(), any(Replacer.class));
+                assertEquals(expectedQuery, sqlCaptor.getValue());
+
+                // Verify that the invocation of queryObjects contains the proper query parameters so that query is properly parametrized with the expected values
+                verify(handler).queryObjects(any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), queryParametersCaptor.capture());
+                assertEquals(Arrays.asList(inClause), queryParametersCaptor.getValue());
+            }
+        }
+    }
+
+    @Test
+    public void testSqlGetHandler_queryParametersAreProperlyPassedToQueryObjectsMethodWithRqlParameters() throws Exception {
+
+        Url url = new Url("http://localhost:8080/api/lift/us/plugins/updatexml(rand(),concat(CHAR(126),user(),CHAR(126)),null)?name1=asdf&name2=qwer");
+
+        String passedQuery = "updatexml(rand(),concat(CHAR(126),user(),CHAR(126)),null)";
+        String collectionName = "plugins";
+        String expectedQuery = " SELECT * FROM " + collectionName + " WHERE plugins IN (?,?,?,?,?) ";
+        String[] inClause = {"updatexml(rand()", "concat(CHAR(126)", "user()", "CHAR(126))", "null)"};
+
+        // Setup request object and sql table objects to properly reflect
+        Request request = new Request(url, "GET", new HashMap<>(), new HashMap<>(), "");
+        Service.ApiMatch apiMatch = new Service.ApiMatch(api, endpoint, "GET", request.getUrl(), "http://localhost:8080/api/lift/us/", collectionName + "/" + passedQuery + "/");
+        Collection collection = new Collection();
+        Entity entity = new Entity(new Table(sqlDb, collectionName));
+        Attribute attribute = new Attribute();
+
+        attribute.setName(collectionName);
+        entity.setKey(attribute);
+        collection.setEntity(entity);
+        request.setApiMatch(apiMatch);
 
         // Mocked methods that return local objects used in the future
         when(chain.getService()).thenReturn(service);
         when(service.getDb(request.getApi(), request.getCollectionKey(), SqlDb.class)).thenReturn(sqlDb);
         when(sqlDb.getConnection()).thenReturn(connection);
-        when(Rql.getRql(sqlDb.getType())).thenReturn(sqlRql);
-        when(api.getCollection(request.getCollectionKey(), SqlDb.class)).thenReturn(collection);
-        when(Sql.getInClauseStr(J.explode(",", Sql.check(request.getEntityKey())))).thenReturn(passedQuery);
 
-        // When we call the service method on the mocked handler actually call the real method
-        doCallRealMethod().when(handler).service(service, api, endpoint, action, chain, request, response);
+        try (MockedStatic<Rql> rqlMockedStatic = mockStatic(Rql.class)) {
+            try (MockedStatic<Sql> sqlMockedStatic = mockStatic(Sql.class)) {
 
-        // More mocked methods that return objects reused locally
-        when(sqlRql.asCol("plugins")).thenReturn(collectionName);
-        when(Sql.check(collectionName)).thenReturn(collectionName);
-        when(Sql.getQuestionMarkStr(inClause)).thenReturn("?,?,?,?,?");
-        when(sqlRql.createStmt(any(), any(), any(), any(Replacer.class))).thenReturn(stmt);
-        when(chain.getConfig("maxRows", 0)).thenReturn(0);
+                rqlMockedStatic.when(() -> Rql.getRql(sqlDb.getType())).thenReturn(sqlRql);
 
-        // Call the actual method on the mocked handler
-        handler.service(service,api,endpoint,action,chain,request,response);
+                when(api.getCollection(request.getCollectionKey(), SqlDb.class)).thenReturn(collection);
+                sqlMockedStatic.when(() -> Sql.getInClauseStr(J.explode(",", Sql.check(request.getEntityKey())))).thenReturn(passedQuery);
 
-        // Verify that the passed in query contains no SQL functions
-        verify(sqlRql).createStmt(sqlCaptor.capture(), any(), any(), any(Replacer.class));
-        assertEquals(expectedQuery, sqlCaptor.getValue());
+                // When we call the service method on the mocked handler actually call the real method
+                doCallRealMethod().when(handler).service(service, api, endpoint, action, chain, request, response);
 
-        // Verify that the invocation of queryObjects contains the proper query parameters so that query is properly parametrized with the expected values
-        verify(handler).queryObjects(any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), queryParametersCaptor.capture());
-        assertEquals(Arrays.asList(inClause), queryParametersCaptor.getValue());
+                // More mocked methods that return objects reused locally
+                when(sqlRql.asCol("plugins")).thenReturn(collectionName);
+                sqlMockedStatic.when(() -> Sql.check(collectionName)).thenReturn(collectionName);
+                sqlMockedStatic.when(() -> Sql.getQuestionMarkStr(inClause)).thenReturn("?,?,?,?,?");
+                when(sqlRql.createStmt(any(), any(), any(), any(Replacer.class))).thenReturn(stmt);
+                when(chain.getConfig("maxRows", 0)).thenReturn(0);
+
+                // Call the actual method on the mocked handler
+                handler.service(service, api, endpoint, action, chain, request, response);
+
+                // Verify that the passed in query contains no SQL functions
+                verify(sqlRql).createStmt(sqlCaptor.capture(), any(), rqlParamsCaptor.capture(), any(Replacer.class));
+                assertEquals(expectedQuery, sqlCaptor.getValue());
+                // Assert that we are passing in the correct rql parameters parsed from the URL
+                assertEquals(request.getParams(), rqlParamsCaptor.getValue());
+
+                // Verify that the invocation of queryObjects contains the proper query parameters so that query is properly parametrized with the expected values
+                verify(handler).queryObjects(any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), queryParametersCaptor.capture());
+                assertEquals(Arrays.asList(inClause), queryParametersCaptor.getValue());
+            }
+        }
+
     }
 }
