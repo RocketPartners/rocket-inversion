f0751e2d282d94a96fcd79bd5d115cf231907343
masonchester <masonch32@gmail.com>
2024-05-01 15:40:49 -0400

    Remove unused variables

--- src/test/java/io/rcktapp/api/handler/sql/SqlGetHandlerTest.java
+++ src/test/java/io/rcktapp/api/handler/sql/SqlGetHandlerTest.java
@@ -46,19 +46,7 @@ public class SqlGetHandlerTest {
     Response response;
 
     @Mock
-    Collection collection;
-
-    @Mock
-    Entity entity;
-
-    @Mock
-    Table table;
-
-    @Mock
-    Attribute attribute;
-
-    @Mock
-    Stmt statement;
+    Stmt stmt;
 
     @Mock
     SqlDb sqlDb;
@@ -78,9 +66,6 @@ public class SqlGetHandlerTest {
     @Captor
     ArgumentCaptor<String> sqlCaptor;
     
-    @Captor
-    ArgumentCaptor<Map<String, String>> rqlParamsCaptor;
-
     @Test
     public void testSqlGetHandler_queryParametersAreProperlyPassedToQueryObjectsMethod() throws Exception {
 
@@ -88,44 +73,54 @@ public class SqlGetHandlerTest {
 
         String passedQuery = "updatexml(rand(),concat(CHAR(126),user(),CHAR(126)),null)";
         String collectionName = "plugins";
+        String expectedQuery = " SELECT * FROM " + collectionName + " WHERE plugins IN (?,?,?,?,?) ";
         String[] inClause = {"updatexml(rand()", "concat(CHAR(126)", "user()", "CHAR(126))", "null)"};
 
-        // Setup request object with proper api configuration
+        // Setup request object and sql table objects to properly reflect
         Request request = new Request(url, "GET", new HashMap<>(), new HashMap<>(), "");
         Service.ApiMatch apiMatch = new Service.ApiMatch(api, endpoint, "GET", request.getUrl(), "http://localhost:8080/api/lift/us/", collectionName + "/" + passedQuery + "/");
-        request.setApiMatch(apiMatch);
+        Collection collection = new Collection();
+        Entity entity = new Entity(new Table(sqlDb, collectionName));
+        Attribute attribute = new Attribute();
 
-        mockStatic(Rql.class);
-        mockStatic(Sql.class);
+        attribute.setName(collectionName);
+        entity.setKey(attribute);
+        collection.setEntity(entity);
+        request.setApiMatch(apiMatch);
 
         // Mocked methods that return local objects used in the future
         when(chain.getService()).thenReturn(service);
         when(service.getDb(request.getApi(), request.getCollectionKey(), SqlDb.class)).thenReturn(sqlDb);
         when(sqlDb.getConnection(false)).thenReturn(connection);
-        when(Rql.getRql(sqlDb.getType())).thenReturn(sqlRql);
-        when(api.getCollection(request.getCollectionKey(), SqlDb.class)).thenReturn(collection);
-        when(collection.getEntity()).thenReturn(entity);
-        when(entity.getTable()).thenReturn(table);
-        when(entity.getKey()).thenReturn(attribute);
-        when(attribute.getName()).thenReturn(collectionName);
-        when(Sql.getInClauseStr(J.explode(",", Sql.check(request.getEntityKey())))).thenReturn(passedQuery);
 
-        // When we call the service method on the mocked handler actually call the real method
-        doCallRealMethod().when(handler).service(service, api, endpoint, action, chain, request, response);
+        try (MockedStatic<Rql> rqlMockedStatic = mockStatic(Rql.class);
+             MockedStatic<Sql> sqlMockedStatic = mockStatic(Sql.class)) {
 
-        // More mocked methods that return objects reused locally
-        when(sqlRql.asCol(table.getName())).thenReturn("Plugins");
-        when(Sql.getQuestionMarkStr(inClause)).thenReturn("?,?,?,?,?");
-        when(sqlRql.createStmt(eq(" SELECT * FROM Plugins WHERE null IN (?,?,?,?,?) "), any(), eq(request.getParams()), any(Replacer.class))).thenReturn(statement);
-        when(chain.getConfig("maxRows", 0)).thenReturn(0);
+            rqlMockedStatic.when(() -> Rql.getRql(sqlDb.getType())).thenReturn(sqlRql);
 
-        // Call the actual method on the mocked handler
-        handler.service(service, api, endpoint, action, chain, request, response);
+            when(api.getCollection(request.getCollectionKey(), SqlDb.class)).thenReturn(collection);
+            sqlMockedStatic.when(() -> Sql.getInClauseStr(J.explode(",", Sql.check(request.getEntityKey())))).thenReturn(passedQuery);
 
-        // Verify that the invocation of queryObjects contains the proper parameters.
-        verify(handler).queryObjects(any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), queryParametersCaptor.capture());
+            // When we call the service method on the mocked handler actually call the real method
+            doCallRealMethod().when(handler).service(service, api, endpoint, action, chain, request, response);
 
-        // Assert that the query parameters captured are equal to the expected list of parameters from the passed in URL
-        assertEquals(queryParametersCaptor.getValue(), Arrays.asList(inClause));
+            // More mocked methods that return objects reused locally
+            when(sqlRql.asCol("plugins")).thenReturn(collectionName);
+            sqlMockedStatic.when(() -> Sql.check(collectionName)).thenReturn(collectionName);
+            sqlMockedStatic.when(() -> Sql.getQuestionMarkStr(inClause)).thenReturn("?,?,?,?,?");
+            when(sqlRql.createStmt(any(), any(), any(), any(Replacer.class))).thenReturn(stmt);
+            when(chain.getConfig("maxRows", 0)).thenReturn(0);
+
+            // Call the actual method on the mocked handler
+            handler.service(service, api, endpoint, action, chain, request, response);
+
+            // Verify that the passed in query contains no SQL functions
+            verify(sqlRql).createStmt(sqlCaptor.capture(), any(), any(), any(Replacer.class));
+            assertEquals(expectedQuery, sqlCaptor.getValue());
+
+            // Verify that the invocation of queryObjects contains the proper query parameters so that query is properly parametrized with the expected values
+            verify(handler).queryObjects(any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), queryParametersCaptor.capture());
+            assertEquals(Arrays.asList(inClause), queryParametersCaptor.getValue());
+        }
     }
 }
