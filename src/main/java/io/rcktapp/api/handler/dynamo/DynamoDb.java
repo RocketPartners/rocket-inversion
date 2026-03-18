36cee2b5b92543e79597232ff29d03eca962c071
mason-chester-rp <mason.chester@rocketpartners.io>
2026-03-18 10:36:11 -0400

    RL-1805: fix weird method chaining

--- src/main/java/io/rcktapp/api/handler/dynamo/DynamoDb.java
+++ src/main/java/io/rcktapp/api/handler/dynamo/DynamoDb.java
@@ -100,7 +100,7 @@ public class DynamoDb extends Db
    @Override
    public void bootstrapApi() throws Exception
    {
-      this.dynamoClient = getDynamoClient();
+      this.dynamoClient = getDynamoDbClient();
 
       this.setType("dynamo");
 
@@ -345,8 +345,8 @@ public class DynamoDb extends Db
       return entity;
    }
 
-   public DynamoDbClient getDynamoClient()
-   {
+   public DynamoDbClient getDynamoDbClient() {
+
       if (dynamoClient != null)
       {
          return dynamoClient;
@@ -365,11 +365,6 @@ public class DynamoDb extends Db
 
       dynamoClient = builder.build();
       return dynamoClient;
-   }
-
-   public DynamoDbClient getDynamoDbClient()
-   {
-      return getDynamoClient();
    }
 
    @Override
