7fa9f815c1c7141d04969d12113abb645e6bab5b
mason-chester-rp <mason.chester@rocketpartners.io>
2026-03-25 13:22:21 -0400

    LIFT-1806: upgrade Firehose to AWS SDK v2 and bump version to 0.3.6.15 (0.3.6.x)

--- src/main/java/io/rcktapp/api/handler/firehose/FirehosePostHandler.java
+++ src/main/java/io/rcktapp/api/handler/firehose/FirehosePostHandler.java
@@ -1,13 +1,9 @@
 package io.rcktapp.api.handler.firehose;
 
-import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehose;
-import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseAsync;
-import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseClientBuilder;
-import com.amazonaws.services.kinesisfirehose.model.ListDeliveryStreamsRequest;
-import com.amazonaws.services.kinesisfirehose.model.ListDeliveryStreamsResult;
-import com.amazonaws.services.kinesisfirehose.model.PutRecordBatchRequest;
-import com.amazonaws.services.kinesisfirehose.model.PutRecordBatchResult;
-import com.amazonaws.services.kinesisfirehose.model.Record;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.firehose.FirehoseClient;
+import software.amazon.awssdk.services.firehose.model.PutRecordBatchRequest;
+import software.amazon.awssdk.services.firehose.model.Record;
 import io.forty11.web.js.JSArray;
 import io.forty11.web.js.JSObject;
 import io.rcktapp.api.Action;
@@ -23,11 +19,8 @@ import io.rcktapp.api.SC;
 import io.rcktapp.api.Table;
 import io.rcktapp.api.service.Service;
 
-import java.nio.ByteBuffer;
-import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Future;
 
 /**
  * Posts records to a mapped AWS Kinesis Firehose stream.
@@ -50,70 +43,16 @@ import java.util.concurrent.Future;
  * <code>separator</code> will be appended to the record.
  * <p>
  * The underlying Firehose stream is mapped to the collection name through
- * the FireshoseDb.includeStreams property.
+ * the FirehoseDb.includeStreams property.
  *
  * @author wells
  */
 public class FirehosePostHandler implements Handler
 {
-    public static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static final String lower = upper.toLowerCase();
-    public static final String digits = "0123456789";
-    public static final char[] symbols = (upper + lower + digits).toCharArray();
-    static SecureRandom random = new SecureRandom();
     protected int     batchMax     = 500;
     protected int     fieldCharMax = 10000;
     protected String  separator    = "\n";
     protected boolean prettyPrint  = false;
-
-    public static String randomString(int size)
-    {
-
-        char[] buf = new char[size];
-        for (int idx = 0; idx < buf.length; ++idx)
-            buf[idx] = symbols[random.nextInt(symbols.length)];
-        return new String(buf);
-    }
-
-    public static void main(String[] args) throws Exception
-    {
-        System.out.println(randomString(100));
-
-        AmazonKinesisFirehose firehose = AmazonKinesisFirehoseClientBuilder.defaultClient();
-
-        ListDeliveryStreamsRequest listDeliveryStreamsRequest = new ListDeliveryStreamsRequest();
-        ListDeliveryStreamsResult listDeliveryStreamsResult = firehose.listDeliveryStreams(listDeliveryStreamsRequest);
-        List<String> deliveryStreamNames = listDeliveryStreamsResult.getDeliveryStreamNames();
-        while (listDeliveryStreamsResult.isHasMoreDeliveryStreams())
-        {
-            if (deliveryStreamNames.size() > 0)
-            {
-                listDeliveryStreamsRequest.setExclusiveStartDeliveryStreamName(deliveryStreamNames.get(deliveryStreamNames.size() - 1));
-            }
-            listDeliveryStreamsResult = firehose.listDeliveryStreams(listDeliveryStreamsRequest);
-            deliveryStreamNames.addAll(listDeliveryStreamsResult.getDeliveryStreamNames());
-        }
-
-        System.out.println(deliveryStreamNames);
-
-        for (int i = 0; i < 2000; i++)
-        {
-            List<Record> records = new ArrayList();
-            for (int j = 0; j < 500; j++)
-            {
-                JSObject json = new JSObject("tenantCode", "us", "yearid", 2019, "monthid", 201901, "dayid", 20190110, "locationCode", "asdfasdf", "playerCode", "us-12345667-1", "adId", (i * j), "somecrapproperrty", randomString(500));
-                records.add(new Record().withData(ByteBuffer.wrap((json.toString(false).trim() + "\n").getBytes())));
-            }
-
-            PutRecordBatchRequest put = new PutRecordBatchRequest();
-            put.setDeliveryStreamName("liftck-player9-impression");
-            put.setRecords(records);
-            firehose.putRecordBatch(put);
-
-            System.out.println(i);
-        }
-
-    }
 
     @Override
     public void service(Service service, Api api, Endpoint endpoint, Action action, Chain chain, Request req, Response res) throws Exception
@@ -126,7 +65,7 @@ public class FirehosePostHandler implements Handler
         Table table = col.getEntity().getTable();
         String streamName = table.getName();
 
-        AmazonKinesisFirehoseAsync firehose = ((FirehoseDb) table.getDb()).getFirehoseClient();
+        FirehoseClient firehose = ((FirehoseDb) table.getDb()).getFirehoseClient();
 
         JSObject body = req.getJson();
 
@@ -171,17 +110,17 @@ public class FirehosePostHandler implements Handler
                 if (separator != null && !string.endsWith(separator))
                     string += separator;
 
-                batch.add(new Record().withData(ByteBuffer.wrap(string.getBytes())));
+                batch.add(Record.builder().data(SdkBytes.fromByteArray(string.getBytes())).build());
 
                 if ((i + 1) % batchMax == 0)
                 {
-                    firehose.putRecordBatchAsync(new PutRecordBatchRequest().withDeliveryStreamName(streamName).withRecords(batch));
+                    firehose.putRecordBatch(PutRecordBatchRequest.builder().deliveryStreamName(streamName).records(batch).build());
                     batch = new ArrayList<>();
                 }
             }
 
             if (!batch.isEmpty())
-                firehose.putRecordBatchAsync(new PutRecordBatchRequest().withDeliveryStreamName(streamName).withRecords(batch));
+                firehose.putRecordBatch(PutRecordBatchRequest.builder().deliveryStreamName(streamName).records(batch).build());
         }
         catch (Exception e)
         {
