0b0e9d450e0090a199a3fc947774ec7821639b7b
mason-chester-rp <mason.chester@rocketpartners.io>
2026-03-25 13:02:47 -0400

    LIFT-1807: upgrade RDS IAM auth token generation to AWS SDK v2 (0.3.x)
    Replace com.amazonaws:aws-java-sdk-rds:1.11.542 with software.amazon.awssdk:rds.
    Migrate RdsIamDataSource from RdsIamAuthTokenGenerator/GetIamAuthTokenRequest (v1)
    to RdsUtilities/GenerateAuthenticationTokenRequest (v2).

--- src/main/java/io/rcktapp/api/handler/sql/RdsIamDataSource.java
+++ src/main/java/io/rcktapp/api/handler/sql/RdsIamDataSource.java
@@ -1,11 +1,12 @@
 package io.rcktapp.api.handler.sql;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.regions.DefaultAwsRegionProviderChain;
-import com.amazonaws.services.rds.auth.GetIamAuthTokenRequest;
-import com.amazonaws.services.rds.auth.RdsIamAuthTokenGenerator;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
+import software.amazon.awssdk.services.rds.RdsUtilities;
+import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -24,15 +25,16 @@ public class RdsIamDataSource extends HikariDataSource {
     }
 
     private static String generateAuthToken(String jdbcUrl, String username) {
-        RdsIamAuthTokenGenerator generator = RdsIamAuthTokenGenerator.builder()
-                .credentials(new DefaultAWSCredentialsProviderChain())
-                .region(new DefaultAwsRegionProviderChain().getRegion())
+        Region region = new DefaultAwsRegionProviderChain().getRegion();
+        RdsUtilities utilities = RdsUtilities.builder()
+                .region(region)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
 
-        return generator.getAuthToken(GetIamAuthTokenRequest.builder()
+        return utilities.generateAuthenticationToken(GenerateAuthenticationTokenRequest.builder()
                 .hostname(determineHostname(jdbcUrl))
                 .port(determinePort(jdbcUrl))
-                .userName(username)
+                .username(username)
                 .build());
     }
 
